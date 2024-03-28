db151868d00cf7560a1caf1ac222ef50e1ee9fcb
Akash <sadawarti.akash@gmail.com>
2024-03-28 16:13:56 +0530

    js course update

---
+++ JS-Course/Day-17/day-17.js
@@ -0,0 +1,13 @@
+document.getElementById("demo").innerHTML = "HI Bootcoding";
+
+// 4 Output Methods :
+// 1) innerHTML
+// 2) document.write();
+// 3) console.log;
+// 4) alert();
+
+
+// document.write("Hi Batch003");
+
+console.log(5*6);
+// alert(5+6);
